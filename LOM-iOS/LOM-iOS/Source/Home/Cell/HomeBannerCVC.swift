dda34f3166275dcd2bce5ef95ca9be7bd659558e
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-11-13 16:05:38 +0900

    ➕[ADD] Cell 파일 추가

---
+++ LOM-iOS/LOM-iOS/Source/Home/Cell/HomeBannerCVC.swift
@@ -0,0 +1,17 @@
+//
+//  HomeBannerCVC.swift
+//  LOM-iOS
+//
+//  Created by 김혜수 on 2021/11/13.
+//
+
+import UIKit
+
+class HomeBannerCVC: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
---
+++ LOM-iOS/LOM-iOS/Source/Home/Cell/HomeBestTVC.swift
@@ -0,0 +1,23 @@
+//
+//  HomeBestTVC.swift
+//  LOM-iOS
+//
+//  Created by 김혜수 on 2021/11/13.
+//
+
+import UIKit
+
+class HomeBestTVC: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
