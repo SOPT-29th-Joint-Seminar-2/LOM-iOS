43a496198c74167c29aa60babccd9315b7ded607
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-11-14 03:15:38 +0900

    ➕[ADD] UIColor Extension 추가 (#1)

---
+++ LOM-iOS/LOM-iOS/Configuration/Extension/UIColor +.swift
@@ -0,0 +1,64 @@
+//
+//  UIColor +.swift
+//  LOM-iOS
+//
+//  Created by 김혜수 on 2021/11/14.
+//
+
+import UIKit
+
+extension UIColor {
+
+  @nonobjc class var millieYellow: UIColor {
+    return UIColor(red: 1.0, green: 235.0 / 255.0, blue: 96.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var millieOrange: UIColor {
+    return UIColor(red: 1.0, green: 185.0 / 255.0, blue: 4.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var milliePurple: UIColor {
+    return UIColor(red: 164.0 / 255.0, green: 81.0 / 255.0, blue: 247.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var lightGray2: UIColor {
+    return UIColor(white: 193.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var lightGray1: UIColor {
+    return UIColor(white: 239.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray2: UIColor {
+    return UIColor(white: 111.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray1: UIColor {
+    return UIColor(white: 165.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var gray3: UIColor {
+    return UIColor(white: 85.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var darkGray1: UIColor {
+    return UIColor(white: 61.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var darkGray2: UIColor {
+    return UIColor(white: 36.0 / 255.0, alpha: 1.0)
+  }
+
+  @nonobjc class var black: UIColor {
+    return UIColor(white: 0.0, alpha: 1.0)
+  }
+
+  @nonobjc class var white: UIColor {
+    return UIColor(white: 1.0, alpha: 1.0)
+  }
+
+  @nonobjc class var blue: UIColor {
+    return UIColor(red: 60.0 / 255.0, green: 136.0 / 255.0, blue: 204.0 / 255.0, alpha: 1.0)
+  }
+
+}
