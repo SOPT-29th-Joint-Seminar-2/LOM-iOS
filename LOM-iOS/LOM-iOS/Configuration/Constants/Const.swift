396f04499a46d620a0844858d7d7993de103b85f
hyesuuou <68391767+hyesuuou@users.noreply.github.com>
2021-11-14 14:16:22 +0900

    ➕[ADD] 에셋추가 (#1)

---
+++ LOM-iOS/LOM-iOS/Configuration/Constants/Const.swift
@@ -0,0 +1,12 @@
+//
+//  Const.swift
+//  LOM-iOS
+//
+//  Created by 김혜수 on 2021/11/14.
+//
+
+import Foundation
+
+struct Const {
+    // 구체적인 상수는 Constants 그룹 내부 파일로 각각 Extension 에 작성
+}
---
+++ LOM-iOS/LOM-iOS/Configuration/Constants/Image.swift
@@ -0,0 +1,50 @@
+//
+//  Image.swift
+//  LOM-iOS
+//
+//  Created by 김혜수 on 2021/11/14.
+//
+
+import UIKit
+
+extension Const {
+    
+    struct Image {
+        
+        // MARK: - Home View
+        
+        // navigation bar
+        static let logoIcon = UIImage(named: "logo")!
+        
+        // banner image
+        static let bannerImg1 = UIImage(named: "img_banner1")!
+        static let bannerImg2 = UIImage(named: "img_banner2")!
+        static let bannerImg3 = UIImage(named: "img_banner3")!
+        
+        // 오늘의 책 image
+        static let todayBookImg = UIImage(named: "book6")!
+        
+        // 서점 베스트 Top 10
+        static let bestImg1 = UIImage(named: "book4")!
+        static let bestImg2 = UIImage(named: "book2")!
+        static let bestImg3 = UIImage(named: "book3")!
+        static let bestImg4 = UIImage(named: "book1")!
+        
+        
+        // MARK: - Detail View
+        
+        // navigation bar
+        static let starIcon = UIImage(named: "ic_favotire")!
+        static let moreIcon = UIImage(named: "ic_more")!
+        
+        // 상단 책 정보 부분
+        static let detailBookImg = UIImage(named: "book2")!
+        static let profileGroupIcon = UIImage(named: "ic_profileGroup")!
+        static let reviewIcon = UIImage(named: "ic_review")!
+        static let postIcon = UIImage(named: "ic_post")!
+        
+        // 한 줄 리뷰 부분
+        static let profileIcon = UIImage(named: "Profile")!
+        
+    }
+}
