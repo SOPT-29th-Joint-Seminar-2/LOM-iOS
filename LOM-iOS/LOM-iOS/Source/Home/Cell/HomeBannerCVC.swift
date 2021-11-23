ce2211a255e72c9ef04621f3d6bf5596ed2bea8f
meenyweeny <kmeye0419@gmail.com>
2021-11-24 02:31:40 +0900

    ✨[FEAT] 오늘의 책 View 구현 완료

--- LOM-iOS/LOM-iOS/Source/Home/Cell/HomeBannerCVC.swift
+++ LOM-iOS/LOM-iOS/Source/Home/Cell/HomeBannerCVC.swift
@@ -11,6 +11,10 @@ class HomeBannerCVC: UICollectionViewCell {
     @IBOutlet weak var searchImageView: UIImageView!
     @IBOutlet weak var noticeImageView: UIImageView!
     @IBOutlet weak var menuView: UIView!
+    @IBOutlet weak var nowMenuLabel: UILabel!
+    @IBOutlet weak var nowUnderBarView: UIView!
+    @IBOutlet weak var audioMenuLabel: UILabel!
+    @IBOutlet weak var storyMenuLabel: UILabel!
     
     @IBOutlet weak var optionImageView: UIImageView!
     @IBOutlet weak var bookImageView: UIImageView!
@@ -23,6 +27,26 @@ class HomeBannerCVC: UICollectionViewCell {
     
     override func awakeFromNib() {
         super.awakeFromNib()
+        initializeLayout()
     }
 
+    func initializeLayout(){
+        pageView.clipsToBounds = true
+        pageView.layer.cornerRadius = 16
+        pageView.layer.borderWidth = 1
+        pageView.layer.borderColor = UIColor(red: 0.239, green: 0.239, blue: 0.239, alpha: 1).cgColor
+        pageLabel.textColor = .black
+        promotionLabel.textColor = .black
+        nowMenuLabel.textColor = .black
+        audioMenuLabel.textColor = .black
+        storyMenuLabel.textColor = .black
+    }
+    
+    func setData(page: Int, promote: String, explain: String, bookImage: UIImage){
+        pageLabel.text = "\(page) / 3"
+        pageLabel.font = UIFont.NotoSans(.medium, size: 14)
+        promotionLabel.text = promote
+        explainLabel.text = explain
+        bookImageView.image = bookImage
+    }
 }
--- LOM-iOS/LOM-iOS/Source/Home/HomeVC.swift
+++ LOM-iOS/LOM-iOS/Source/Home/HomeVC.swift
@@ -2,12 +2,26 @@ import UIKit
 
 class HomeVC: UIViewController {
 
+    struct Banners {
+        let title: String
+        let subtitle: String
+        let image: UIImage?
+    }
+
+    struct Books {
+        let title: String
+        let writer: String
+        let image: UIImage?
+    }
+    
     // MARK: @IBOutlets
     @IBOutlet weak var pageScrollView: UIScrollView!
     @IBOutlet weak var pageView: UIView!
     @IBOutlet weak var bannerCollectionView: UICollectionView!
-    @IBOutlet weak var todaysBookButton: UIButton!
+
     @IBOutlet weak var todaysBookView: UIView!
+    @IBOutlet weak var todaysBookLabel: UILabel!
+    
     @IBOutlet weak var todaysBookImageView: UIImageView!
     @IBOutlet weak var timeIconImageView: UIImageView!
     @IBOutlet weak var timeExplainLabel: UILabel!
@@ -19,16 +33,87 @@ class HomeVC: UIViewController {
     @IBOutlet weak var pointOneImageView: UIImageView!
     @IBOutlet weak var pointTwoImageView: UIImageView!
     @IBOutlet weak var pointThreeImageView: UIImageView!
-    @IBOutlet weak var moreInfoOneButton: UIButton!
-    @IBOutlet weak var moreInfoTwoButton: UIButton!
-    @IBOutlet weak var moreInfoThreeButton: UIButton!
+
+    @IBOutlet weak var moreInfoLabelOne: UILabel!
+    @IBOutlet weak var moreInfoLabelTwo: UILabel!
+    @IBOutlet weak var moreInfoLabelThree: UILabel!
+    
     @IBOutlet weak var bestBookListTableView: UITableView!
     @IBOutlet weak var bestBookButton: UIButton!
     
+    var mainBannerContentList: [Banner] = []
+    var bestBookContentList: [Book] = []
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        initMainBannerContentList()
+        initializeTodaysBookLayout()
+        registerXib()
+        bannerCollectionView.dataSource = self
+        bannerCollectionView.delegate = self
+        bannerCollectionView.isPagingEnabled = true
     }
     
+    func registerXib(){
+        let xibName = UINib(nibName: HomeBannerCVC.identifier, bundle: nil)
+        bannerCollectionView.register(xibName, forCellWithReuseIdentifier: HomeBannerCVC.identifier)
+    }
+    
+    func initializeTodaysBookLayout(){
+        
+        todaysBookView.layer.borderColor = UIColor(red: 0.9, green: 0.9, blue: 0.9, alpha: 1).cgColor
+        todaysBookView.layer.cornerRadius = 10
+        todaysBookView.layer.borderWidth = 1
+        
+        todaysBookLabel.text = "오늘의 책"
+        todaysBookLabel.font = UIFont.NotoSans(.bold, size: 20)
+        
+        moreInfoLabelOne.font = UIFont.NotoSans(.medium, size: 13)
+        moreInfoLabelTwo.font = UIFont.NotoSans(.medium, size: 13)
+        moreInfoLabelThree.font = UIFont.NotoSans(.medium, size: 13)
+        
+        moreInfoLabelOne.textColor = .darkGray
+        moreInfoLabelTwo.textColor = .darkGray
+        moreInfoLabelThree.textColor = .darkGray
+        
+        timeExplainLabel.font = UIFont.NotoSans(.regular, size: 12)
+        timeExplainLabel.textColor = .gray1
+        
+        timeLabel.font = UIFont.NotoSans(.medium, size: 14)
+        timeLabel.textColor = .gray3
+        
+        saveBookButton.titleLabel?.font = UIFont.NotoSans(.medium, size: 14)
+    }
+    
+    func initMainBannerContentList(){
+        mainBannerContentList.append(contentsOf: [
+            Banner(title: "SNS로 '진짜 나'를\n찾을 수 있을까", subtitle: "종이책 출간 전에 읽는 화제의 신작", image: Const.Image.bannerImg1),
+            Banner(title: "11주 연속 베스트셀러\n김영하 북클럽 선정도서", subtitle: "11월 8일 커밍순!", image: Const.Image.bannerImg2),
+            Banner(title: "영화와 책을 아우르는\n씨네21 칼럼니스트", subtitle: "이다혜 작가전 보러가기!", image: Const.Image.bannerImg3)
+        ])
+    }
+    
+    func initBestBookContentList(){
+        //from server
+    }
+}
 
+extension HomeVC: UICollectionViewDelegate {
+    
+}
+
+extension HomeVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return mainBannerContentList.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: HomeBannerCVC.identifier, for: indexPath) as? HomeBannerCVC else {return UICollectionViewCell()}
+        
+        let nowData = mainBannerContentList[indexPath.row]
+        
+        cell.setData(page: indexPath.row+1, promote: nowData.title , explain: nowData.subtitle, bookImage: nowData.image)
+        return cell
+    }
+    
 }
