f99f69e19b026eb2ca9cad9d93647eb787d50c45
L-j-h-c <ckrgkswnsgh@naver.com>
2021-11-28 04:09:25 +0900

    ➕[ADD] : 네트워크, 익스텐션 폴더링
    네트워크, 익스텐션 폴더링

--- LOM-iOS/LOM-iOS/Configuration/Network/APIConstants.swift
+++ LOM-iOS/LOM-iOS/Configuration/Network/APIConstants.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+struct APIConstants {
+    static let baseURL = "https://asia-northeast3-library-of-millie.cloudfunctions.net/api"
+    
+    static let getBookInfoURL = baseURL + "/book"
+}
